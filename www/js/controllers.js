0f523eabd4254a608a0e9a362dad895773e11632
Juan Andrés Dávila Herrera <Juan Andrs Dvila Herrera>
2016-07-03 01:45:25 -0500

    primera version

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http) {
+  $scope.result = "";
+  $http.get('http://resta-ldupatd.c9users.io:8080/reservation')
+    .success(function(data, status, headers,config){
+      console.log('data success');
+      console.log(data); // for browser console
+      $scope.result = data; // for UI
+    })
+    .error(function(data, status, headers,config){
+      console.log('data error');
+    })
+    .then(function(result){
+      things = result.data;
+    });
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
